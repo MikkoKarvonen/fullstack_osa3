830bf5694bcdabe18b1ad5e4aa25545bfdf0ec0c
MikkoKarvonen <mkarvonen34@gmail.com>
2019-04-29 17:00:06 +0300

    Lisätty tehtävä 3.3

--- puhelinluettelo/index.js
+++ puhelinluettelo/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-let notes = [
+let numbers = [
     {
         id: 1,
         name: 'Timo Testaaja',
@@ -20,12 +20,18 @@ let notes = [
 ]
 
 app.get('/info', (req, res) => {
-    res.send(`<p>Puhelinluettelossa ${notes.length} henkilön tiedot</p>`+
-             `<p>${new Date()}</p>`)
+    res.send(`<p>Puhelinluettelossa ${numbers.length} henkilön tiedot</p>` +
+        `<p>${new Date()}</p>`)
 })
 
-app.get('/api/persons', (req, res) => {
-    res.json(notes)
+app.get('/api/persons/', (req, res) => {
+    res.json(numbers)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const number = numbers.find(number => number.id === id)
+    number ? res.json(number) : res.status(404).end()
 })
 
 const PORT = 3001
