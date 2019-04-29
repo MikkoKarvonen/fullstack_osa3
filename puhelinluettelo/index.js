ae51e7ae35cc1b1dac5223bffd653921ae6fedff
MikkoKarvonen <mkarvonen34@gmail.com>
2019-04-29 16:48:15 +0300

    Lisätty tehtävä 3.1

---
+++ puhelinluettelo/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+    {
+        id: 1,
+        name: 'Timo Testaaja',
+        number: '040-1234567'
+    },
+    {
+        id: 2,
+        name: 'Maija Meikäläinen',
+        number: '040-1122334'
+    },
+    {
+        id: 3,
+        name: 'Roope Ankka',
+        number: '09-123123'
+    },
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
